90b4e125e9d7f191c66f3d44105132336a7cc10b
Siavash Soleymani <siavash.soleymani@apple.com>
2020-12-25 18:08:56 -0800

    clean install

--- R/signif_up.R
+++ R/signif_up.R
@@ -1,3 +1,19 @@
+#' round up to most significant digit
+#'
+#' _round up_ a number to the most meaningful digits per scale of the number
+#'
+#' @param x numeric
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' signif_up(1320.65) # 1400
+#' signif_up(132.5) # 140
+#' signif_up(32.6)  # 40
+#' signif_up(3.6)   # 4
+#' signif_up(0.0654)  # 0.07
+#' signif_up(c(1320.654, 3675.675, 5.7, 0.76))
 signif_up = function(x) {
   num_string <- format(x, scientific = TRUE)
   n <- strsplit(num_string, "e")
--- R/summarise_vr_by_grp_fn.R
+++ R/summarise_vr_by_grp_fn.R
@@ -1,5 +1,31 @@
+#' group by and summarise with multiple functions
+#'
+#' @param dta input dataset
+#' @param grp group by vars defined by vars()
+#' @param vr variable to be summarised defined by vars()
+#' @param fn a named list of functions
+#' @param ... any other input listed fn functions
+#'
+#' @return grouped and summarised dataset
+#' @export
+#' @importFrom dplyr vars %>% group_by summarise_at
+#' @importFrom stats median
+#'
+#' @examples
+#' summarise_vr_by_grp_fn(
+#' dta = iris,
+#' grp = vars(Species),
+#' vr = vars("Sepal.Length", "Petal.Width"),
+#' fn = list(avg = mean, median = median)
+#' )
+#' summarise_vr_by_grp_fn(
+#' dta = iris,
+#' grp = vars(Species),
+#' vr = vars(Sepal.Length, Petal.Width),
+#' fn = list(avg = mean, median = median)
+#' )
 summarise_vr_by_grp_fn = function(dta, grp = vars(), vr = vars(), fn = list(mean = mean, median = median), ...){
   dta %>%
-    group_by(!!!grp) %>%
-    summarise_at(.vars = vr, .funs = fn, ...)
+    dplyr::group_by(!!!grp) %>%
+    dplyr::summarise_at(.vars = vr, .funs = fn, ...)
 }
